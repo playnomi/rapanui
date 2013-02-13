5c839662dd0211b7548b28d5a33ecc2f8d839355
MinPont Chien <minpont@gmail.com>
2013-02-13 15:03:58 -0800

    Fixed bug with RNGroup:setVisible --- works with text now and boundary condition

--- rapanui-sdk/RNGroup.lua
+++ rapanui-sdk/RNGroup.lua
@@ -325,16 +325,16 @@ end
 
 
 function RNGroup:setVisible(value)
-    for i = 0, self.numChildren - 1 do
+    for i = 1, self.numChildren do
         local anObject = self.displayObjects[i]
         if anObject ~= nil then
             if anObject.getType ~= nil then
                 if anObject:getType() ~= "RNButton" then
                     if anObject ~= nil and type(anObject.getProp) ~= "table" and anObject:getProp() ~= nil then
-                        anObject.visible = value
+                        anObject:setVisible(value)
                     end
                 else
-                    anObject.visible = value
+                    anObject:setVisible(value)
                 end
             end
         end
